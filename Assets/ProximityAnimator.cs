4021307d6d29ee0dbfb8107e9a36d079464cb151
geo2405 <georgiana310@yahoo.com>
2025-10-05 16:01:53 +0300

    lab 1 geo

---
+++ Assets/ProximityAnimator.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class ProximityAnimator : MonoBehaviour
+{
+    public Transform otherBear;   // set manually in Inspector
+    public float attackDistance = 0.2f; // distance in Unity meters
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (otherBear == null || anim == null) return;
+
+        float dist = Vector3.Distance(transform.position, otherBear.position);
+
+        if (dist <= attackDistance)
+        {
+            anim.SetTrigger("Attack");
+        }
+        else
+        {
+            anim.ResetTrigger("Attack");
+        }
+
+    }
+}
